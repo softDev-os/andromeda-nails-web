687c52888089599a623980760b3194a3ebd00f52
softDev <softdev.iso@gmail.com>
2026-03-20 14:29:41 -0500

    base

---
+++ src/data/categories.ts
@@ -0,0 +1,2 @@
+// List of categories for blog posts
+export const CATEGORIES = ['Acrilicas', 'Comercial', 'Pedicure', 'Polygel', 'Semi', 'Spa'] as const
---
+++ src/data/site.config.ts
@@ -0,0 +1,19 @@
+interface SiteConfig {
+	author: string
+	title: string
+	description: string
+	lang: string
+	ogLocale: string
+	shareMessage: string
+}
+
+export const siteConfig: SiteConfig = {
+	author: 'Juan Fontalvo (softDev) - Web Develoment',
+	title: 'Andromeda Nails - Manicura & Pedicura',
+	description:
+		'Explora variados diseños de uñas acrílicas, techgel y comerciales. Inspírate en nuestra galería de tendencias y manicuras.',
+	lang: 'es',
+	ogLocale: 'es',
+	shareMessage:
+		'Explora nuestra colección de diseños de uñas acrílicas, techgel y comerciales. Encuentra inspiración en nuestra variada galería de imágenes de manicuras y tendencias de belleza.' // Message to share a post on social media
+}
